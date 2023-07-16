d761254cbd3052ca8e631be96c5c16a4f4b95264
AndreaRaffaelli <121181476+AndreaRaffaelli@users.noreply.github.com>
2023-07-17 00:17:24 +0200

    Update InserimentoPrenotazioneServlet.java

--- src/Servlet/InserimentoPrenotazioneServlet.java
+++ src/Servlet/InserimentoPrenotazioneServlet.java
@@ -28,72 +28,54 @@ public class InserimentoPrenotazioneServlet extends HttpServlet {
 	{ }
 	
 	@Override	
-	public void doPost(HttpServletRequest request,
-            HttpServletResponse response)
-    throws ServletException, IOException
-	{
-        String nameSurname = request.getParameter("name-surname");
-        String startDate = request.getParameter("start-date");
-        String endDate = request.getParameter("end-date");
-        String position = request.getParameter("position");
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        String nameSurname = request.getParameter("name-surname");
+	        String startDate = request.getParameter("start-date");
+	        String endDate = request.getParameter("end-date");
+		String position = request.getParameter("position");
 		int nPosizione= Integer.parseInt(position);
 		String objectData = request.getParameter("object-data");
-              
-
-
 		// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate parsedStartDate = LocalDate.parse(startDate);
 		LocalDate parsedEndDate = LocalDate.parse(endDate);
-
-
-        System.out.println("Nome e cognome: " + nameSurname);
-        System.out.println("Data inizio: " + startDate);
-        System.out.println("Data fine: " + endDate);
-        System.out.println("Posizione: " + position);
-        System.out.println("Oggetti inseriti: " + objectData);
-
-        
-        // Inoltra la richiesta alla CalendarioServlet
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/calendarioServlet"); // Assumi che il nome della mappatura sia "/calendario" per la CalendarioServlet
-        request.setAttribute("dataInizio", parsedStartDate);
-        request.setAttribute("dataFine", parsedEndDate);
-        dispatcher.include(request, response);
-
+			
+	        System.out.println("Nome e cognome: " + nameSurname);
+	        System.out.println("Data inizio: " + startDate);
+	        System.out.println("Data fine: " + endDate);
+	        System.out.println("Posizione: " + position);
+	        System.out.println("Oggetti inseriti: " + objectData);
+	
+	        // Inoltra la richiesta alla CalendarioServlet
+	        RequestDispatcher dispatcher = request.getRequestDispatcher("/calendarioServlet"); // Assumi che il nome della mappatura sia "/calendario" per la CalendarioServlet
+	        request.setAttribute("dataInizio", parsedStartDate);
+	        request.setAttribute("dataFine", parsedEndDate);
+	        dispatcher.include(request, response);
+			
 		System.out.println("Continuo");
-
 		Periodo newPeriodo= Periodo.nuovoPeriodo(parsedStartDate, parsedEndDate);
-
 		Archivio a= Archivio.getInstace();
 		Posizione p = a.getPlanimetria().getPosizione(nPosizione);
 		boolean isOccupato = p.isOccupato(newPeriodo);
-
 		if(isOccupato){
 			request.setAttribute("tipoErrore", "Errore durante l'inserimento della prenotazione");
-            request.setAttribute("messaggioErrore", "La posizione selezionata non è disponibile per il periodo selezionato.");
-            dispatcher = request.getRequestDispatcher("/errore.jsp");
-            dispatcher.forward(request, response);
+	        	request.setAttribute("messaggioErrore", "La posizione selezionata non è disponibile per il periodo selezionato.");
+	        	dispatcher = request.getRequestDispatcher("/errore.jsp");
+	        	dispatcher.forward(request, response);
 		}
 		Prenotazione newPrenotazione = new Prenotazione();
 		String[] righe = objectData.split(System.lineSeparator());
-		for(String riga : righe)
-		{
+		for(String riga : righe) {
 			String[] parts = riga.split("-");
 			String nomeO = parts[0].trim(); // Nome (prima parte)
 			String quantitaPart = parts[1].trim(); // Seconda parte (contenente "Quantita: 2")
-
+	
 			// Estrazione del numero dalla seconda parte
 			String quantitaStr = quantitaPart.substring(quantitaPart.indexOf(":") + 1).trim();
 			int quantita = Integer.parseInt(quantitaStr);
 			Oggetto oggetto = a.getOggetto(nomeO);
 			newPrenotazione.addOggetto(oggetto, quantita);
 		}
-		
-
-		a.addPrenotazione(newPrenotazione);
-		
+		a.addPrenotazione(newPrenotazione);		
 	}
 	
-
-	
-
 }
