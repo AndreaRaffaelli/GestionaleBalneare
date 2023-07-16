613ee7c06b827d69e3be7ec033e364a6288d7e67
AndreaRaffaelli <121181476+AndreaRaffaelli@users.noreply.github.com>
2023-07-16 23:43:45 +0200

    Add files via upload

--- src/modello/Planimetria.java
+++ src/modello/Planimetria.java
@@ -3,8 +3,6 @@ package modello;
 import java.util.HashMap;
 import java.util.Map;
 
-//Ho aggiunto setPosizione
-
 // Classe Planimetria
 public class Planimetria {
 	private int colonne;
@@ -14,6 +12,7 @@ public class Planimetria {
 
 	private Planimetria() {
 		posizioni = new HashMap<Integer, Posizione>();
+		setMappa();
 	}
 
 	public static Planimetria getInstance() {
@@ -24,24 +23,23 @@ public class Planimetria {
 	}
 
 	public Posizione getPosizione(int id) {
-		if (id < 0 || id > righe * colonne) {
-			return this.posizioni.get(id);
-		} else
-			return null;
-	}
-
-	public void setPosizione(int id, Posizione p) {
-		if (id > 0 && id < righe * colonne) {
-			this.posizioni.put(id, p);
-		}
+		return this.posizioni.getOrDefault(id, new PosizioneNonPrenotabile(id));
 	}
 
 	/***
-	 *  Imposta che una cella non è prenotabile. Se viene chiesto di ottenere quella cella
-	 *  viene restituito PosizioneNonPrenotabile, che estende Posizione
+	 * Imposta che una cella non è prenotabile. Se viene chiesto di ottenere quella
+	 * cella viene restituito PosizioneNonPrenotabile, che estende Posizione
 	 * 
 	 */
 	public boolean setNonPrenotabile(int id) {
+		if (this.posizioni.containsKey(id)) {
+			return false;
+		}
+		this.posizioni.put(id, new PosizioneNonPrenotabile(id));
+		return true;
+	}
+
+	public boolean setPrenotabile(int id) {
 		if (this.posizioni.containsKey(id)) {
 			return false;
 		}
@@ -53,7 +51,16 @@ public class Planimetria {
 	 * Implementazione per impostare la mappa della planimetria Ritorna true se
 	 * l'operazione ha avuto successo, altrimenti false Lettura XML
 	 */
-	public boolean setMappa() {
+	private boolean setMappa() {
+		this.colonne = 9;
+		this.righe = 9;
+		int id = 0;
+		for (int i = 0; i < colonne * righe; i++) {
+			if (i != 5) {
+				id++;
+				posizioni.put(i, new Posizione(id));
+			}
+		}
 		return true;
 	}
 
@@ -69,7 +76,7 @@ public class Planimetria {
 		if (y < 0 || y > this.colonne)
 			return -1;
 		else
-			return righe * colonne;
+			return x * y;
 
 	}
 
